edbe473af9d5c97ffe152b0a4a3e79113c5c1ced
Elly2018 <34325707+Elly2018@users.noreply.github.com>
2025-02-21 06:50:53 +0800

    update

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, session } from 'electron';
+import { app, autoUpdater, BrowserWindow, dialog, session } from 'electron';
 import { join } from 'path';
 import './client/client';
 import { eventInit } from './event';
@@ -6,6 +6,11 @@ import { menu_client } from './menu';
 import './plugins/i18n';
 
 export let mainWindow:BrowserWindow | undefined = undefined
+
+const server = 'https://your-deployment-url.com'
+const url = `${server}/update/${process.platform}/${app.getVersion()}`
+
+autoUpdater.setFeedURL({ url })
 
 function createWindow () {
   mainWindow = new BrowserWindow({
@@ -35,6 +40,10 @@ function createWindow () {
   if(process.env.NODE_ENV === 'development'){
     mainWindow?.webContents.openDevTools()
   }
+
+  setInterval(() => {
+    autoUpdater.checkForUpdates()
+  }, 60000)
 }
 
 app.whenReady().then(() => {
@@ -62,4 +71,24 @@ app.whenReady().then(() => {
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 });
+
+autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+  const dialogOpts:Electron.MessageBoxOptions = {
+    type: 'info',
+    buttons: ['Restart', 'Later'],
+    title: 'Application Update',
+    message: process.platform === 'win32' ? releaseNotes : releaseName,
+    detail:
+      'A new version has been downloaded. Restart the application to apply the updates.'
+  }
+
+  dialog.showMessageBox(mainWindow!, dialogOpts).then((returnValue) => {
+    if (returnValue.response === 0) autoUpdater.quitAndInstall()
+  })
+})
+
+autoUpdater.on('error', (message) => {
+  console.error('There was a problem updating the application')
+  console.error(message)
+})
 
