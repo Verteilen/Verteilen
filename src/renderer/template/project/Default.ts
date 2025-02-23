92830bee466a87c161588acc129767dc27edb83a
Elly2018 <34325707+Elly2018@users.noreply.github.com>
2025-02-23 23:22:07 +0800

    add example stuff

--- src/renderer/template/project/Default.ts
+++ src/renderer/template/project/Default.ts
@@ -1,5 +1,17 @@
-import { Project } from "../../interface"
+import { Parameter, Project } from "../../interface"
 
 export const GetDefaultProjectTemplate = (r:Project):Project => {
+    const para:Parameter = {
+            numbers: [
+                { name: "n1", value: 120 }
+            ],
+            strings: [
+                { name: "s1", value: "Hello World" }
+            ],
+            booleans: [
+                { name: "b1", value: false }
+            ],
+        }
+        r.parameter = para
     return r
 }
