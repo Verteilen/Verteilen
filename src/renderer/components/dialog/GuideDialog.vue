154fb5e67a811315ef946f915002bd7e0b58c315
Elly2018 <34325707+Elly2018@users.noreply.github.com>
2025-04-04 11:46:43 +0800

    Added guide viewer

---
+++ src/renderer/components/dialog/GuideDialog.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+    const modal = defineModel<boolean>({ required: true })
+</script>
+
+<template>
+    <v-dialog width="800" v-model="modal" class="text-white text-center">
+        <v-card>
+
+        </v-card>
+    </v-dialog>
+</template>
