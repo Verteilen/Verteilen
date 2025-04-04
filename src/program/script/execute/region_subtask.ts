892a7288aefcd6f2ce9a1b57d1d715bfafd6f0dc
Elly2018 <34325707+Elly2018@users.noreply.github.com>
2025-04-04 11:57:31 +0800

    Optimized the share script, prevent copy duplicate scripts

--- src/program/script/execute/region_subtask.ts
+++
@@ -1,9 +0,0 @@
-import { ExecuteManager } from "../execute_manager"
-
-export class Region_Subtask {
-    target:ExecuteManager
-
-    constructor(_target:ExecuteManager){
-        this.target = _target
-    }
-}
