59fd3075aeb280d368b02aca826bdeb2cefdd9be
Elly2018 <34325707+Elly2018@users.noreply.github.com>
2025-04-04 12:02:56 +0800

    Recover some file and modify some path for renderer components and classes

---
+++ src/renderer/debugger.ts
@@ -0,0 +1,21 @@
+import { checkifElectron } from "./platform"
+
+const isElectron = checkifElectron()
+let feedback:Function | undefined = undefined
+
+export const set_feedback = (_feedback:Function) => {
+    feedback = _feedback
+}
+
+export const messager = (msg:string, tag?:string) => {
+    const text = tag != undefined ? `[${tag}] ${msg}` : `[後台訊息] ${msg}`
+    console.log(text)
+    if (feedback != undefined) feedback(text)
+}
+
+export const messager_log = (msg:string, tag?:string) => {
+    messager(msg, tag)
+    if(!isElectron) return
+    window.electronAPI.send('message', msg, tag);
+    
+}
---
+++ src/renderer/plugins/vuetify.ts
@@ -0,0 +1,24 @@
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import 'vuetify/styles'
+
+export const vuetify = createVuetify({ 
+    components, 
+    directives, 
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi,
+        },
+    },
+    theme: { 
+        defaultTheme: 'dark'
+    }
+})
+
+export const GetColor = (c:string) => {
+    return vuetify.theme.current.value.colors[c]
+}
