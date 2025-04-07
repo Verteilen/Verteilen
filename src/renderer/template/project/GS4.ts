8ff3a1f3169efcbfb58de9c95ab12449773c13ea
Elly2018 <34325707+Elly2018@users.noreply.github.com>
2025-04-07 14:30:06 +0800

    Added core support for GS template

--- src/renderer/template/project/GS4.ts
+++ src/renderer/template/project/GS4.ts
@@ -396,8 +396,8 @@ export const GetFUNIQUE_GS4ProjectTemplate_PlyList = ():Task => {
         description: "生成 ply 序列!!",
         cronjob: false,
         cronjobKey: "",
-        multi: false,
-        multiKey: "",
+        multi: true,
+        multiKey: "core",
         properties: [],
         jobs: [
             sequenceJob
@@ -423,8 +423,8 @@ export const GetFUNIQUE_GS4ProjectTemplate_Blend1 = ():Task => {
         description: "Ply 透明度調整",
         cronjob: true,
         cronjobKey: "frameCount",
-        multi: false,
-        multiKey: "",
+        multi: true,
+        multiKey: "core",
         properties: [
             {
                 name: 'frame_index',
@@ -455,8 +455,8 @@ export const GetFUNIQUE_GS4ProjectTemplate_Blend2 = ():Task => {
         description: "Ply 多序包裝成單序",
         cronjob: true,
         cronjobKey: "frameCount",
-        multi: false,
-        multiKey: "",
+        multi: true,
+        multiKey: "core",
         properties: [],
         jobs: [
             mergeJob
@@ -469,13 +469,13 @@ export const GetFUNIQUE_GS4ProjectTemplate = (r:Project):Project => {
     const para:Parameter = {
         canWrite: true,
         containers: [
-            { name: "frameCount", value: 20, type: DataType.Number, runtimeOnly: false, hidden: false },
-            { name: "iframe_gap", value: 5, type: DataType.Number, runtimeOnly: false, hidden: false },
-            { name: "lut_thread", value: 5, type: DataType.Number, runtimeOnly: false, hidden: false },
-            { name: "group_size", value: 20, type: DataType.Number, runtimeOnly: false, hidden: false },
-            { name: "blend", value: 4, type: DataType.Number, runtimeOnly: false, hidden: false },
-            { name: "contribute", value: 1, type: DataType.Number, runtimeOnly: false, hidden: false },
-            { name: "iframe_size", value: 0, type: DataType.Number, runtimeOnly: false, hidden: false },
+            { name: "frameCount", value: 50, type: DataType.Number, runtimeOnly: false, hidden: false },
+            { name: "iframe_gap", value: 3, type: DataType.Number, runtimeOnly: false, hidden: false },
+            { name: "core", value: 5, type: DataType.Number, runtimeOnly: false, hidden: false },
+            { name: "group_size", value: 15, type: DataType.Number, runtimeOnly: false, hidden: false },
+            { name: "blend", value: 5, type: DataType.Number, runtimeOnly: false, hidden: false },
+            { name: "contribute", value: 0, type: DataType.Number, runtimeOnly: false, hidden: false },
+            { name: "iframe_size", value: 17, type: DataType.Number, runtimeOnly: false, hidden: false },
             { name: "denoise", value: 0, type: DataType.Number, runtimeOnly: false, hidden: false },
             { name: "density_util", value: 2000, type: DataType.Number, runtimeOnly: false, hidden: false },
             { name: "iframe_iteration", value: 7000, type: DataType.Number, runtimeOnly: false, hidden: false },
