182c1d13fdc32eb1a358e2d1548a95faa12395c0
ZhuElly <34325707+Elly2018@users.noreply.github.com>
2025-05-27 23:14:30 +0800

    Converting

--- src/renderer/template/js/GS4/PlyDone.ts
+++ src/renderer/template/js/GS4/PlyDone.ts
@@ -1,45 +1,44 @@
 export const FUNIQUE_GS4_PLYDone:string = `
-local root = env.getstring("root")
-local after_folder = env.getstring("after")
-local output_folder = env.getstring("output")
+root = env.getstring("root");
+after_folder = env.getstring("after");
+output_folder = env.getstring("output");
+blend = env.getnumber("blend");
 
-local blend = env.getnumber("blend")
-local iframe_gap = env.getnumber("iframe_gap")
-local iframe_iteration = env.getnumber(iframe_iteration)
-local finetune_iteration = env.getnumber(finetune_iteration)
+iframe_gap = env.getnumber("iframe_gap");
+iframe_iteration = env.getnumber(iframe_iteration);
+finetune_iteration = env.getnumber(finetune_iteration);
 
-o.createdir(output_folder.."/final")
-o.createdir(output_folder.."/trans")
+o.createdir(\`\${output_folder}/final\`);
+o.createdir(\`\${output_folder}/trans\`);
 
-for i=1,blend,1 do
-    local output_folder_seq = output_folder.."/raw/Sequence_"..tostring( (i-1) * iframe_gap )
-    local source_folder = root.."/"..after_folder.."/".."BLEND_"..tostring((i - 1) * iframe_gap).."_I/checkpoint"
-    o.createdir(output_folder_seq)
+for(i = 0; i < blend; i++){
+    output_folder_seq = \`\${output_folder}/raw/Sequence_\${i * iframe_gap}\`;
+    source_folder = \`\${root}/\${after_folder}/BLEND_\${i * iframe_gap}_I/checkpoint\`;
+    o.createdir(output_folder_seq);
 
-    local allfolder = split(o.listdir(source_folder), "\\n")
-    local count = 0
+    allfolder = o.listdir(source_folder);
+    count = 0;
 
-    for key,value in pairs(allfolder) do
-        local prefix = source_folder.."/"..value.."/point_cloud/"
-        local suffix = "/point_cloud.ply"
-        local plyPaths = { 
-            prefix.."iteration_"..tostring(iframe_iteration)..suffix, 
-            prefix.."iteration_"..tostring(finetune_iteration)..suffix 
-        }
-        local exists = { 
+    allfolder.forEach((value, key) => {
+        prefix = \`\${source_folder}/\${value}/point_cloud/\`;
+        suffix = "/point_cloud.ply";
+        plyPaths = [ 
+            \`\${prefix}iteration_\${iframe_iteration}suffix\`, 
+            \`\${prefix}iteration_\${finetune_iteration}suffix\`
+        ];
+        exists = [
             o.exist(plyPaths[1]), 
             o.exist(plyPaths[2]) 
-        }
-        for key2,value2 in pairs(exists) do
-            if value2 then
-                o.copyfile(plyPaths[key2], output_folder_seq.."/"..value..".ply")
+        ];
+        exists.forEach((value2, key2) => {
+            if (value2) {
+                o.copyfile(plyPaths[key2], \`\${output_folder_seq}/\${value}.ply\`)
                 count = count + 1
-                goto finish
-            end
-        end
-        ::finish::
-    end
+                return
+            }
+        })
+    })
 
-    m.messager_log("Total file copy: "..tostring(count)..", to path: "..output_folder_seq)
-end
+    m.messager_log(\`Total file copy: \${count}, to path: \${output_folder_seq}\`)
+}
 `
--- src/renderer/template/js/GS4/Prepare.ts
+++ src/renderer/template/js/GS4/Prepare.ts
@@ -1,52 +1,52 @@
 export const FUNIQUE_GS4_PREPARE:string = `
 console.log("Env Init");
 
-root = env.getstring("root")
-prepare_folder = env.getstring("prepare")
-before_folder = env.getstring("before")
-after_folder = env.getstring("after")
-output_folder = env.getstring("output")
+root = env.getstring("root");
+prepare_folder = env.getstring("prepare");
+before_folder = env.getstring("before");
+after_folder = env.getstring("after");
+output_folder = env.getstring("output");
 
-iframe_gap = env.getnumber("iframe_gap")
-CAM = env.getstring("CAM")
-images = env.getstring("images")
-sparse = env.getstring("sparse")
+iframe_gap = env.getnumber("iframe_gap");
+CAM = env.getstring("CAM");
+images = env.getstring("images");
+sparse = env.getstring("sparse");
 
-o.deletedir(root.."/"..before_folder)
-o.deletedir(root.."/"..after_folder)
-o.deletedir(output_folder)
-o.createdir(root.."/"..before_folder)
-o.createdir(root.."/"..after_folder)
-o.createdir(output_folder)
+o.deletedir(root.."/"..before_folder);
+o.deletedir(root.."/"..after_folder);
+o.deletedir(output_folder);
+o.createdir(root.."/"..before_folder);
+o.createdir(root.."/"..after_folder);
+o.createdir(output_folder);
 
-console.log("Get CAM list")
-prepare_folders = o.listdir(\`\${root}/\${prepare_folder}/CAM\`)
+console.log("Get CAM list");
+prepare_folders = o.listdir(\`\${root}/\${prepare_folder}/CAM\`);
 
-cam_size = prepare_folders.length
-console.log(\`Get CAM count: \${cam_size}\`)
+cam_size = prepare_folders.length;
+console.log(\`Get CAM count: \${cam_size}\`);
 
-minus = 0
+minus = 0;
 
-frame_size = 0
+frame_size = 0;
 if (cam_size > 0){
-    f1_files = o.listfile(\`\${root}/\${prepare_folder}/CAM/\${prepare_folders[1]}\`)
-    frame_size = f1_files.length
+    f1_files = o.listfile(\`\${root}/\${prepare_folder}/CAM/\${prepare_folders[1]}\`);
+    frame_size = f1_files.length;
 }
-console.log(\`Get Frame count: \${frame_size}\`)
+console.log(\`Get Frame count: \${frame_size}\`);
 
-console.log("Copy sparse folder")
-o.copydir(root.."/"..prepare_folder.."/"..sparse, root.."/"..before_folder.."/"..sparse)
+console.log("Copy sparse folder");
+o.copydir(\`\${root}/\${prepare_folder}/\${sparse}\`, \`\${root}/\${before_folder}/\${sparse}\`);
 
 for (key = 1; key < frame_size; key++){
-    o.createdir(root.."/"..before_folder.."/"..tostring(key - minus).."/images")
+    o.createdir(\`\${root}/\${before_folder}/\${key - minus}/images\`);
     for (key2 = 1; key2 < cam_size; key2++){
-        from = root.."/"..prepare_folder.."/"..CAM.."/"..string.format("%04d", key2).."/"..string.format("%04d", key2).."_"..string.format("%06d", key)..".jpg"
-        to = root.."/"..before_folder.."/"..tostring(key - minus).."/images/"..string.format("%04d", key2)..".jpg"
-        o.copyfile(from, to)
-        console.log_log("Copy file: '"..from.."'   to  '"..to.."'")
+        from = \`\${root}/\${prepare_folder}/CAM/\${key2.toString().padString(4, "0")}/\${key.toString().padString(6, "0")}.jpg\`;
+        to = \`\${root}/\${before_folder}/\${key - minus}/images/\${key2.toString().padString(4, "0")}.jpg\`;
+        o.copyfile(from, to);
+        console.log_log(\`Copy file: \${from}   to   \${to}\`);
     }
 }
 
-env.setnumber("frameCount", frame_size)
-env.setnumber("iframe_size", math.ceil(frame_size / iframe_gap))
+env.setnumber("frameCount", frame_size);
+env.setnumber("iframe_size", math.ceil(frame_size / iframe_gap));
 `
