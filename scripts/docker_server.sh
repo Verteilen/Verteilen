58f8b9a01a43ce9c6c9c16d45e5e58dcb7159b41
ZhuElly <34325707+Elly2018@users.noreply.github.com>
2025-06-04 23:28:23 +0800

    Added log

--- scripts/docker_server.sh
+++ scripts/docker_server.sh
@@ -3,12 +3,12 @@
 if [ $1="--fast" ]
 then
 
-docker build -t e87870823/compute_tool_server -f ./scripts/docker_server_fast.Dockerfile .
+docker build -t e87870823/compute_tool_server -f ./scripts/docker_server_fast.Dockerfile . --progress=plain
 read -p "Press enter to continue"
 
 else 
 
-docker build -t e87870823/compute_tool_server -f ./scripts/docker_server.Dockerfile .
+docker build -t e87870823/compute_tool_server -f ./scripts/docker_server.Dockerfile . --progress=plain
 read -p "Press enter to continue"
 
 fi
