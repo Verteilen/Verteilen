016450367b4f74a246837687d12c3da09d86a7b8
ZhuElly <34325707+Elly2018@users.noreply.github.com>
2025-06-10 05:16:01 +0800

    Fixed build script for docker

--- scripts/docker_node.sh
+++ scripts/docker_node.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-if [ $1="--fast" ]
+if [[ "$1" == "--fast" ]];
 then
+
+echo "fast mode"
 
 docker build -t e87870823/compute_tool_node -f ./scripts/docker_node_fast.Dockerfile . --progress=plain
 read -p "Press enter to continue"
 
 else
+
+echo "full mode"
 
 docker build -t e87870823/compute_tool_node -f ./scripts/docker_node.Dockerfile . --progress=plain
 read -p "Press enter to continue"
