c5e801bda3cc4511c78f611ba0218c1a4a17a7d2
ZhuElly <34325707+Elly2018@users.noreply.github.com>
2025-08-10 11:51:41 +0800

    Update share for template module for future update

---
+++ src/template/interface/base.ts
@@ -0,0 +1,74 @@
+// ========================
+//                           
+//      Share Codebase     
+//                           
+// ========================
+import { DataType, DataTypeBase } from "./enum"
+
+export interface ParameterConfigTrigger {
+    types: Array<DataTypeBase>
+}
+
+export interface ParameterContainer {
+    s?: boolean
+    name: string
+    meta?: any
+    config?: ParameterConfigTrigger
+    type: DataType
+    hidden: boolean
+    runtimeOnly: boolean
+    value: any
+}
+
+export interface Property {
+    name: string
+    expression: string
+    deep?: number
+}
+
+export interface Parameter {
+    uuid: string
+    title: string
+    canWrite: boolean
+    containers: Array<ParameterContainer>
+}
+
+export interface Job {
+    s?: boolean
+    index?:number
+    uuid: string
+    runtime_uuid?: string
+    category: number
+    type: number
+    script: string
+    string_args: Array<string>
+    number_args: Array<number>
+    boolean_args: Array<boolean>
+}
+
+export interface Task {
+    uuid: string
+    title: string
+    description: string
+    setupjob: boolean
+    cronjob: boolean
+    cronjobKey: string
+    multi: boolean
+    multiKey: string
+    properties: Array<Property>
+    jobs: Array<Job>
+}
+
+export interface Project {
+    uuid: string
+    title: string
+    description: string
+    parameter_uuid: string
+    parameter?: Parameter
+    task: Array<Task>
+}
+
+export interface Node {
+    ID: string
+    url: string
+}
