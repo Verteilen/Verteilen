750f988c9cc0a72caa0c72c532343c463d0437a0
ZhuElly <34325707+Elly2018@users.noreply.github.com>
2025-08-23 00:50:48 +0800

    Added console encry ssl

--- src/server/index.ts
+++ src/server/index.ts
@@ -14,16 +14,17 @@ import { EventInit } from './event_http'
 let wsServer: ws.Server | undefined = undefined
 let app:express.Express | undefined = undefined
 let httpss:https.Server<any> | undefined = undefined
+let wss:https.Server<any> | undefined = undefined
 
 const webport = backendEvent.PortAvailable(WebPORT)
 const socketport = backendEvent.PortAvailable(ConsolePORT)
 
-const get_pem = ():Promise<[string, string]> => {
+const get_pem = (express:boolean):Promise<[string, string]> => {
     return new Promise<[string, string]>((resolve) => {
         const pemFolder = path.join(os.homedir(), DATA_FOLDER, 'pem')
         if(!fs.existsSync(pemFolder)) fs.mkdirSync(pemFolder)
-        const clientKey = path.join(pemFolder, "express_clientkey.pem")
-        const certificate = path.join(pemFolder, "express_certificate.pem")
+        const clientKey = path.join(pemFolder, express ? "express_clientkey.pem" : "console_clientkey.pem")
+        const certificate = path.join(pemFolder, express ? "express_certificate.pem" : "console_certificate.pem")
         if(!fs.existsSync(clientKey) || !fs.existsSync(certificate)){
             pem.createCertificate({selfSigned: true}, (err, keys) => {
                 fs.writeFileSync(clientKey, keys.clientKey, { encoding: 'utf8' })
@@ -40,18 +41,19 @@ export const main = async (middle?:any):Promise<[express.Express | undefined, ws
     return new Promise<[express.Express | undefined, ws.Server | undefined]>(async (resolve) => {
         {
             const p = await webport
-            const pems = await get_pem()
+            const pems = await get_pem(true)
             app = express()
             httpss = https.createServer({ key: pems[0], cert: pems[1], minVersion: 'TLSv1.2', maxVersion: 'TLSv1.3' }, app)
-            httpss.addListener('upgrade', (req, res, head) => console.log('UPGRADE:', req.url))
             EventInit(app, middle)
             httpss.listen(p, () => {
-                console.log(Chalk.greenBright(`server run at ${p}`))
+                console.log(Chalk.greenBright(`https server run at ${p}`))
             })
             backendEvent.Root()
         }
         {
             const p = await socketport
+            const pems = await get_pem(false)
+            wss = https.createServer({ key: pems[0], cert: pems[1], minVersion: 'TLSv1.2', maxVersion: 'TLSv1.3' }, app)
             wsServer = new ws.Server({path: '/server', port: p})
             console.log(Chalk.greenBright(`websocket server run at ${p}`))
             wsServer.on('connection', (ws, request) => {
@@ -66,6 +68,9 @@ export const main = async (middle?:any):Promise<[express.Express | undefined, ws
                 ws.on('close', () => {
                     backendEvent.DropConsoleConsole(ws)
                 })
+            })
+            wss.listen(p, () => {
+                console.log(Chalk.greenBright(`ws server run at ${p}`))
             })
         }
 
